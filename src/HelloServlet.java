62e963920031d05ae322ef6c578353f22bd1f0ac
Julia0296 <ms.julia.0296@mail.ru>
2016-10-12 14:56:44 +0300

    HelloServlet_First_Task

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +10,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public class HelloServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.write("<html>");
+        pw.write("<head>");
+        pw.write("<title>HelloServlet</title>");
+        pw.write("</head>");
+        pw.write("<body>");
+        pw.write("<h1>HelloServlet</h1>");
+        pw.write("</body>");
+        pw.write("</html>");
     }
 }
